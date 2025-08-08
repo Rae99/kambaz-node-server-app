a5a43272ba8d1a72b284c768d804f839b36f285f
Rae99 <ruibantian812@gmail.com>
2025-08-08 09:21:38 +0800

    6.2.5

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ import CourseRoutes from './Kambaz/Courses/routes.js';
 import ModuleRoutes from './Kambaz/Modules/routes.js';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
+import mongoose from 'mongoose';
 import 'dotenv/config';
+const CONNECTION_STRING =
+  process.env.DATABASE_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kambaz';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 app.use(
@@ -16,8 +20,8 @@ app.use(
     credentials: true,
     origin: process.env.CLIENT_URL || 'http://localhost:5173',
   })
- );
- 
+);
+
 // support cookies
 // restrict cross origin resource
 // sharing to the react application
